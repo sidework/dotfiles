37a2879f8c85787e9f73da1552768951ab9d35a0
sidework <sidework@sideworktech.com>
2024-12-21 15:18:47 -0500

    a little more cleanup, notes to fix keybase

--- scripts/programs/keybase.sh
+++ scripts/programs/keybase.sh
@@ -5,3 +5,10 @@ curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
 sudo apt install ./keybase_amd64.deb
 rm ./keybase_amd64.deb
 run_keybase
+
+#key is 656D16C7
+#apt-key list
+#fix with:
+#apt-key export 656D16C7 | sudo gpg --dearmour -o /usr/share/keyrings/keybase.gpg
+#add to /etc/apt/sources.list.d/keybase.list 
+#[arch=amd64 signed-by=/usr/share/keyrings/keybase.gpg]
